7d9583b481e54062c69267c9bb922b16cab4f297
Shubhangi Bagal <shubhangibgl134@gmail.com>
2019-06-20 15:30:48 +0530

    Added config

--- registration/registrationFormHandler.js
+++ registration/registrationFormHandler.js
@@ -20,6 +20,49 @@ class RegistrationViewHandlerHasiruDala {
         return FormElementsStatusHelper
             .getFormElementsStatusesWithoutDefaults(new RegistrationViewHandlerHasiruDala(), individual, formElementGroup);
     }
+
+    @WithRegistrationStatusBuilder
+    pleaseSelectTheClassCourse([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Are you studying presently?").is.yes;
+    }
+
+    @WithRegistrationStatusBuilder
+    pleaseSelectTheReasonSForDiscontinuation([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Are you studying presently?").is.no;
+    }
+
+    @WithRegistrationStatusBuilder
+    pleaseEnterDetailsOfYourSkillVocationalTraining([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Please select the class/course").containsAnswerConceptName("Other: Skill/Vocational Training");
+    }
+
+    @WithRegistrationStatusBuilder
+    pleaseEnterDetails([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Do you have any physical disability?").is.yes;
+    }
+
+    @WithRegistrationStatusBuilder
+    doYouHaveADoctorsCertificateIndicatingThePercentageOfDisability([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Do you have any physical disability?").is.yes;
+    }
+
+    @WithRegistrationStatusBuilder
+    pleaseUploadTheDoctorsCertificate([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Do you have any physical disability?").is.yes;
+    }
+
+    @WithRegistrationStatusBuilder
+    occupationalIdNumber([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Do you have an Occupational ID card?").is.yes;
+    }
+
+    @WithRegistrationStatusBuilder
+    uploadPhotoOfTheId([], statusBuilder) {
+    statusBuilder.show().when.valueInRegistration("Do you have an Occupational ID card?").is.yes;
+    }
+
+
+
 }
 
 module.exports = {RegistrationViewHandlerHasiruDala};
