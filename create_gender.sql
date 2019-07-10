e65bd19936697542fb466bc54642f5ae38eee33b
Vinod Deolal <vinoddeolal@gmail.com>
2019-07-10 18:09:59 +0530

    0000 | create audit using org-admin id

--- create_gender.sql
+++ create_gender.sql
@@ -1,16 +1,14 @@
-set role hasiru;
-
 insert into gender(uuid, name, version, audit_id, is_voided, organisation_id)
-select uuid_generate_v4(), 'Male', 0, create_audit(), false, id
+select uuid_generate_v4(), 'Male', 0, create_audit((select id from users where username = 'admin@hasiru')), false, id
 from organisation
 where name = 'Hasiru Dala';
 
 insert into gender(uuid, name, version, audit_id, is_voided, organisation_id)
-select uuid_generate_v4(), 'Female', 0, create_audit(), false, id
+select uuid_generate_v4(), 'Female', 0, create_audit((select id from users where username = 'admin@hasiru')), false, id
 from organisation
 where name = 'Hasiru Dala';
 
 insert into gender(uuid, name, version, audit_id, is_voided, organisation_id)
-select uuid_generate_v4(), 'Other', 0, create_audit(), false, id
+select uuid_generate_v4(), 'Other', 0, create_audit((select id from users where username = 'admin@hasiru')), false, id
 from organisation
 where name = 'Hasiru Dala';
