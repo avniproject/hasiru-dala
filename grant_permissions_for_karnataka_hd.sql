7856daa3376a83587bfb95b5391108279ebb0a24
Hiren Thacker <hithacker@gmail.com>
2020-08-11 16:43:51 +0530

    #48 | Give permisssions to hd views to hasiru_karnataka role

---
+++ grant_permissions_for_karnataka_hd.sql
@@ -0,0 +1,12 @@
+set role openchs;
+
+GRANT ALL ON hasiru_registration_details TO hasiru_karnataka;
+GRANT ALL ON hasiru_locations_view TO hasiru_karnataka;
+GRANT ALL ON hasiru_age_group TO hasiru_karnataka;
+GRANT ALL ON hasiru_marial_status_view TO hasiru_karnataka;
+GRANT ALL ON hasiru_category_view TO hasiru_karnataka;
+GRANT ALL ON hd_registration_base_view TO hasiru_karnataka;
+GRANT ALL ON hd_scheme_status_base_view TO hasiru_karnataka;
+GRANT ALL ON hd_training_view TO hasiru_karnataka;
+
+reset role;
