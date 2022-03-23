b2ac55271403ab607ed392c34c742f4ba66cbe82
Nupoor Khandelwal <37863085+nupoorkhandelwal@users.noreply.github.com>
2022-03-23 12:21:59 +0530

    Update migration.sql

--- karnataka/migration.sql
+++ karnataka/migration.sql
@@ -15,3 +15,26 @@ jsonb_build_object('47233ab8-3190-4de6-8e09-572e5d1480cc', '249ee1c0-9a18-4854-8
 last_modified_date_time = current_timestamp + ((i.id % 4000) * interval '1 millisecond'),
 last_modified_by_id = (select id from users where username = 'nupoork@hasiru_ka_uat')
 where i.observations ->> '36f29633-4d58-46e4-9f67-c2ef47d48032' is null;
+
+
+--Script to migrate Health encounter questions to Registration
+set role hasiru_ka_uat;
+-- 1809	Registration
+-- 3036	Health form Details group_id = 6066
+-- 3880	Health and Sanitation Information
+
+update form_element
+set form_element_group_id = 3880,
+    display_order = display_order + 11,
+    last_modified_date_time = current_timestamp + (id % 10000) * '1 milliseconds'::interval,
+    last_modified_by_id = (select id from users where username = 'nupoork@hasiru_ka_uat')
+where form_element_group_id = 6066;
+
+-- Script to migrate health encounter observations to individual registration
+update individual i
+set observations = i.observations || enc.observations,
+    last_modified_date_time = current_timestamp + ((i.id % 4000) * interval '1 millisecond'),
+    last_modified_by_id = (select id from users where username = 'nupoork@hasiru_ka_uat')
+from encounter enc
+where i.subject_type_id = 271 and encounter_type_id = 1199
+  and enc.observations is not null;
